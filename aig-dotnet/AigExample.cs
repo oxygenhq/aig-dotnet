274428884f694dd26702daace3d12d415ae98846
romovs <romovs@gmail.com>
2021-06-01 21:27:09 +0300

    Use different scenarios

--- aig-dotnet/AigExample.cs
+++ aig-dotnet/AigExample.cs
@@ -11,105 +11,82 @@ namespace AIGExample
     public class AigExample : CBTest
     {
         [TestMethod]
-        [Description("ביטוח נסיעות לחול")]
-        public void AbroadTravelInsurance()
+        [Description("לינקים")]
+        public void Links()
         {
-            StartStep("ביטוח נסיעות לחול");
-
             StartStep("דף הבית");
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
             _driver.Navigate().GoToUrl("https://www.aig.co.il/");
             _driver.Manage().Window.Maximize();
             EndStep("דף הבית");
 
-            StartStep("נסיעות לחול");
-            _driver.FindElement(By.XPath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/div[1]/div/div/div/div[2]/ul/li[4]/a/div[2]")).Click();
-            EndStep("נסיעות לחול");
+            StartStep("אודות AIG ישראל");
+            _driver.FindElement(By.XPath("//a[contains(text(),'אודות AIG ישראל')]")).Click();
+            EndStep("אודות AIG ישראל");
 
-            StartStep("לחצו כאן למעבר לרכישת ביטוח נסיעות לחול אונליין");
-            _driver.FindElement(By.XPath("//*[@title=\"לחצו כאן למעבר לרכישת ביטוח נסיעות לחול אונליין\"]")).Click();
-            EndStep("לחצו כאן למעבר לרכישת ביטוח נסיעות לחול אונליין");
+            StartStep("כל הסיבות לבחור ב-AIG");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//a[contains(text(),'כל הסיבות לבחור ב-AIG')]")).Click();
+            EndStep("כל הסיבות לבחור ב-AIG");
 
-            StartStep("האם רכשת בעבר ביטוח נסיעות ");
-            Thread.Sleep(2000);
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromMinutes(1));
-            _driver.FindElement(By.XPath("//button[@id=\"closeBtn\"]")).Click();
+            StartStep("חברי הנהלה");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//a[contains(text(),'חברי הנהלה')]")).Click();
+            EndStep("חברי הנהלה");
 
-            Thread.Sleep(2000);
-
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-            try
-            {
-                _driver.FindElement(By.XPath("//button[@id=\"closeBtn\"]")).Click();
-                Thread.Sleep(2000);
-            } 
-            catch (Exception)
-            {
-            }
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
-
-            EndStep("האם רכשת בעבר ביטוח נסיעות ");
-
-            StartStep("אירופה");
-            _driver.FindElement(By.Id("chkDestination156")).Click();
-            EndStep("אירופה");
-
-            StartStep("שוויץ");
-            var el = _driver.FindElement(By.Id("chkDestination156"));
-            el.Click();
-            el.SendKeys("שוויץ");
-            EndStep("שוויץ");
-
-            StartStep("האם כל הנוסעים מגיל 16 ומעלה השלימו את הליך החיסון ");
-            _driver.FindElement(By.Id("coronaVaccinatedBtnYes")).Click();
-            EndStep("האם כל הנוסעים מגיל 16 ומעלה השלימו את הליך החיסון ");
-
-            EndStep("ביטוח נסיעות לחול");
+            StartStep("אמנת השירות");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//a[contains(text(),'אמנת השירות')]")).Click();
+            EndStep("אמנת השירות");
         }
 
         [TestMethod]
-        [Description("שירות לקוחות ותביעות")]
-        public void ShirutLakuhot()
+        [Description("ביטוח רכב")]
+        public void BituahRehev()
         {
-            StartStep("חיפוש מוסך - שירות לקוחות ותביעות");
-
             StartStep("דף הבית");
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
             _driver.Navigate().GoToUrl("https://www.aig.co.il/");
             _driver.Manage().Window.Maximize();
             EndStep("דף הבית");
 
-            StartStep("שירות לקוחות ותביעות");
-            _driver.FindElement(By.Id("menu-1117-link")).Click();
-            EndStep("שירות לקוחות ותביעות");
+            StartStep("ביטוח רכב");
+            Thread.Sleep(1400);
+            _driver.FindElement(By.XPath("(//a[@id='types_products_single_link']/div[2])[1]")).Click();
+            EndStep("ביטוח רכב");
 
-            StartStep("איתור בעלי מקצוע");
-            _driver.FindElement(By.XPath("//strong[text()=\"איתור בעלי מקצוע\"]")).Click();
-            EndStep("איתור בעלי מקצוע");
+            StartStep("לחצו כאן להצעה אונליין");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//a[contains(text(),'לחצו כאן להצעה אונליין')]")).Click();
+            EndStep("לחצו כאן להצעה אונליין");
 
-            StartStep("איתור מוסכים ובעלי מקצוע");
-            _driver.FindElement(By.XPath("//span[text()=\"איתור מוסכים ובעלי מקצוע\"]")).Click();
-            EndStep("איתור מוסכים ובעלי מקצוע");
+            StartStep("לקבלת הצעה");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//button[@id='btn-get-offer']/span")).Click();
+            EndStep("לקבלת הצעה");
 
-            StartStep("מוסכים");
-            var sel = _driver.FindElement(By.Id("filter-garages-type"));
-            var selectElement = new SelectElement(sel);
-            selectElement.SelectByText("מוסכים");
-            EndStep("מוסכים");
+            StartStep("הזנת פרטים");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.Id("img-male")).Click();
+            _driver.FindElement(By.Id("insuranceDate_1")).Click();
+            _driver.FindElement(By.Id("firstName")).SendKeys("משה");
+            _driver.FindElement(By.Id("lastName")).SendKeys("זוכמיר");
+            EndStep("הזנת פרטים");
 
-            StartStep("עיר");
-            sel = _driver.FindElement(By.Id("filter-garages-city"));
-            selectElement = new SelectElement(sel);
-            selectElement.SelectByText("רמת השרון");
-            EndStep("עיר");
+            StartStep("המשך");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.Id("continue-btn")).Click();
+            EndStep("המשך");
 
-            StartStep("התמחות");
-            sel = _driver.FindElement(By.Id("filter-garages-murshe"));
-            selectElement = new SelectElement(sel);
-            selectElement.SelectByText("כללי");
-            EndStep("התמחות");
+            StartStep("מספר טלפון");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.Id("mobilePhone")).SendKeys("0544501591");
+            EndStep("מספר טלפון");
 
-            EndStep("חיפוש מוסך - שירות לקוחות ותביעות");
+            StartStep(" 2 המשך");
+            Thread.Sleep(1000);
+            _driver.FindElement(By.XPath("//button[@id='continue-btn']/span")).Click();
+            EndStep(" 2 המשך");
         }
 
         [TestMethod]
